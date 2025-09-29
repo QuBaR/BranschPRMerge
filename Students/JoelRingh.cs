34a3d6d7f1fcf4a0f5fcbb2e034aadff9f0928e9
Joel Ringh <joel.ringh@gmail.com>
2025-09-29 10:15:15 +0200

    Created Joel Ringh Class

---
+++ Students/JoelRingh.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranschPRMerge.Students
+{
+    internal class JoelRingh
+    {
+        public static string Name => "Joel Ringh";
+
+        public static string SayHello() => $"Hej mitt namn är {Name}";
+
+    }
+}
