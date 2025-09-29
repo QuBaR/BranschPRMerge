8bd4dd6269fe6e05241095c21da0c463e0f7fcca
frans <frans123@live.se>
2025-09-29 10:25:38 +0200

    Refactor FransBashir class namespace and structure
    Updated the namespace from `BranschPRMerge-` to `BranschPRMerge.Students;` for better organization.
    Added necessary `using` directives and redefined the `FransBashir` class with proper formatting.
    Functionality remains unchanged, but overall code clarity and structure have been improved.

--- FransBashir.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BranschPRMerge
-{
-    public static class FransBashir
-    {
-        public static string Name => "Frans Bashir";
-        public static string SayHello() => $"Hej från {Name}!";
-        
-    }
-}
---
+++ Students/FransBashir.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranschPRMerge.Students;
+public static class FransBashir
+{
+    public static string Name => "Frans Bashir";
+    public static string SayHello() => $"Hej från {Name}!";
+}
