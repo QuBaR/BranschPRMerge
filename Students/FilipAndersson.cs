2a0e52f583b6f393aa9e6fb8d6521d2e95738aba
Filip Andersson <andersson1_filip@hotmail.com>
2025-09-29 10:31:09 +0200

    Commit

--- Students/FilipAndersson.cs
+++ Students/FilipAndersson.cs
@@ -6,12 +6,10 @@ using System.Threading.Tasks;
 
 namespace BranschPRMerge.Students
 {
-    internal class FilipAndersson
-    {
+ 
         public static class Filip
         {
             public static string Name => "Filip Andersson";
-            public static string SayHello() => $"Hej från {Filip}!";
+            public static string SayHello() => $"Hej från {Name}!";
         }
-    }
 }
