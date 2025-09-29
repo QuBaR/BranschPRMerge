a1139996fd2b377edcf485dfd413b1e4d86a5a30
Filip Andersson <andersson1_filip@hotmail.com>
2025-09-29 10:17:12 +0200

    Commit av Filip

---
+++ Students/FilipAndersson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranschPRMerge.Students
+{
+    internal class FilipAndersson
+    {
+        public static class Filip
+        {
+            public static string Name => "Filip Andersson";
+            public static string SayHello() => $"Hej från {Filip}!";
+        }
+    }
+}
