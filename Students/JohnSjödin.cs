d1d299dcb7932f4a21785858e669bb5937ee470e
John Sjödin <sjodin.j@gmail.com>
2025-09-29 10:16:33 +0200

    Add JohnSjödin class in Students/

---
+++ Students/JohnSjödin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranschPRMerge.Students
+{
+    internal class JohnSjödin
+    {
+        public static string Name => "John Sjödin";
+        public static string SayHello() => $"Hej från {Name}!";
+    }
+}
