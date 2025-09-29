92576991deb52853055aa8950975ea971a96f62a
ihab samar <ihab-samar@hotmail.com>
2025-09-29 10:50:19 +0200

    Ihab

---
+++ Students/Ihab.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranschPRMerge.Students
+{
+    public static class Ihab
+    {
+        public static string Name => "Ihab"; 
+        public static string SayHello () => $"Hej från {Name}!";
+    }
+}
