92d2eb3e6ce4b10a31ac0a33c4cb4e1a84a172ad
hassan sulaiman <Sullaimanhassan8@gmail.com>
2025-09-29 13:11:05 +0200

    Added hassas suleiman to students

---
+++ Students/HassanSuleiman.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranschPRMerge.Students
+{
+    public static class HassanSuleiman
+    {
+        public static string Name => "Hassan Suleiman";
+        public static string SayHello() => $"Hej från {Name}!";
+    }
+}
