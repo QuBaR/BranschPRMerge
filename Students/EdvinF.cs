a4d5b57ec81ae36d8b1290fa9f45b9d432b73fc7
fogelt <fogeltt@gmail.com>
2025-09-29 10:16:40 +0200

    Added EdvinF class

---
+++ Students/EdvinF.cs
@@ -0,0 +1,8 @@
+namespace BranschPRMerge.Students
+{
+    public static class EdvinF
+    {
+        public static string Name => "Edvin F";
+        public static string SayHello() => $"Hej från {Name}!";
+    }
+}
