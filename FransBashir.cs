af3de06dc7a0f751e818f296f69de679ec39a88b
frans <frans123@live.se>
2025-09-29 10:15:02 +0200

    Add FransBashir class in BranschPRMerge namespace
    Created a new namespace `BranschPRMerge` containing a static class `FransBashir`.
    This class features a static property `Name` that returns "Frans Bashir" and a
    static method `SayHello()` that provides a greeting message using the `Name` property.

---
+++ FransBashir.cs
@@ -0,0 +1,9 @@
+﻿namespace BranschPRMerge
+{
+    public static class FransBashir
+    {
+        public static string Name => "Frans Bashir";
+        public static string SayHello() => $"Hej från {Name}!";
+        
+    }
+}
