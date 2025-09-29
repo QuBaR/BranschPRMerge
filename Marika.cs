fddf62ba67deb8fe7a68c825517cc10f1b469ccd
MarikaRomeo <marika.romeo.syne25lin@edu.tucsweden.se>
2025-09-29 10:19:21 +0200

    Add Marika class to BranschPRMerge namespace
    Introduces a new class named `Marika` with several using directives for common namespaces. This sets the foundation for future functionality within the class.
    
    Add Marika class with nested RobertJansz class
    
    Introduces a new `Marika` class in the `BranschPRMerge` namespace.
    Includes a nested static class `RobertJansz` with a static property
    `Name` returning "Marika Romeo" and a method `SayHello` for a
    greeting message.

---
+++ Marika.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranschPRMerge
+{
+    internal class Marika
+    {
+
+    public static class RobertJansz
+    {
+        public static string Name => "Marika Romeo";
+        public static string SayHello() => $"Hej från {Name}!";
+    }
+}
+}
